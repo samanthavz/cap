ee35a2a92d85a4e4688761072963f60fc4b9826c
samantha van zandwijk <samanthavanzandwijk@gmail.com>
2021-02-24 13:41:25 +0100

    added content

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,21 @@
+// button click function
+
 let p = document.getElementById("p");
 let button = document.getElementById("button");
 let check = 1;
-
+var clicked = false;
 
 
 button.addEventListener("click", function() {
-    p.innerHTML = "<strong> Sam </strong> clicked the button";
+    if (clicked) {
+        return
+    };
+    clicked = true;
+    p.innerHTML = "<strong style='color:red'> Sam </strong> clicked the button";
     console.log("check");
     setTimeout(function() {
         countdown();
+        clicked = false;
     }, 5000);    
 });
 
@@ -16,3 +23,15 @@ function countdown() {
     console.log("check2");
     p.innerHTML = "Press the button if you know the answer";
 };
+
+//background image png/jpg check
+
+var fs = require('fs');
+var files = fs.readdirSync('../images/background*');
+
+var body = document.getElementById("body");
+
+body.style.backgroundImage = files[0];
+
+
+// background-image: url("../images/background.jpg");
